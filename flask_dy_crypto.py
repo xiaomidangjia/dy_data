ab49eba649910bfb410f673cedecedd09098201a
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2023-08-02 22:39:57 +0800

    Add files via upload

--- flask_dy_crypto.py
+++ flask_dy_crypto.py
@@ -60,29 +60,31 @@ def dy_crypto_chen():
             api_value = 'correct'
             # 读取历史开单记录
             p = []
-            with open("/root/upload_date/csv_from_chen.csv", 'r', encoding="UTF-8") as fr:
+            with open("/root/upload_data/csv_from_chen.csv", 'r', encoding="UTF-8") as fr:
                 reader = csv.reader(fr)
                 for index, line in enumerate(reader):
                     if index == 0:
                         continue
                     p.append(line)
             res_data = pd.DataFrame(p)
-            res_data['crypto_name'] = res_data.iloc[:,0]
-            res_data['crypto_direction'] = res_data.iloc[:,1]
-            res_data['crypto_type'] = res_data.iloc[:,2]
-            res_data['crypto_open'] = res_data.iloc[:,3]
-            res_data['crypto_win'] = res_data.iloc[:,4]
-            res_data['crypto_loss'] = res_data.iloc[:,5]
+            res_data['crypto_id'] = res_data.iloc[:,0]
+            res_data['crypto_name'] = res_data.iloc[:,1]
+            res_data['crypto_direction'] = res_data.iloc[:,2]
+            res_data['crypto_type'] = res_data.iloc[:,3]
+            res_data['crypto_open'] = res_data.iloc[:,4]
+            res_data['crypto_win'] = res_data.iloc[:,5]
+            res_data['crypto_loss'] = res_data.iloc[:,6]
             res_data = res_data.reset_index(drop=True)
             price_res = []
             for i in range(len(res_data)):
+                crypto_id = res_data['crypto_id'][i]
                 crypto_name = res_data['crypto_name'][i]
                 crypto_direction = res_data['crypto_direction'][i]
                 crypto_type = res_data['crypto_type'][i]
                 crypto_open = res_data['crypto_open'][i]
                 crypto_win = res_data['crypto_win'][i]
                 crypto_loss = res_data['crypto_loss'][i]
-                price_res.append({'crypto_name':crypto_name,'crypto_direction':crypto_direction,'crypto_type':crypto_type,'crypto_open':crypto_open,'crypto_win':crypto_win,'crypto_loss':crypto_loss})
+                price_res.append({'crypto_id':crypto_id,'crypto_name':crypto_name,'crypto_direction':crypto_direction,'crypto_type':crypto_type,'crypto_open':crypto_open,'crypto_win':crypto_win,'crypto_loss':crypto_loss})
 
 
             res_dict = {'value':api_value,'price_res':str(price_res)}
